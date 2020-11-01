c3f76ffe65b08787bf423d7144b7019b3ad7f82b
Shek <Noorlanedu@gmail.com>
2020-11-01 16:29:41 -0500

    Shek initial commit 2

--- src/main/java/LastGitHubClass.java
+++ src/main/java/LastGitHubClass.java
@@ -2,5 +2,6 @@ public class LastGitHubClass {
     public static void main(String[] args) {
         System.out.println("Shek");
         System.out.println("Getting fun");
+        System.out.println("I have learned a lot about Git and GitHub");
     }
 }
