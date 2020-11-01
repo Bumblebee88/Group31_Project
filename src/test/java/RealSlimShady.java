94d1375f367b64f326d92ff6587ddfd164587837
Bumblebee88 <uros.andrijanic@gmail.com>
2020-11-01 16:38:38 -0600

    BumbleBee was here

--- src/test/java/RealSlimShady.java
+++ src/test/java/RealSlimShady.java
@@ -1,5 +1,6 @@
 public class RealSlimShady {
     public static void main(String[] args) {
         System.out.println("I have learned a lot about Git and GitHub ");
+        System.out.println("hello again");
     }
 }
