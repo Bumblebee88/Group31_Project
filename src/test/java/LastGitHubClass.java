306f50bb3913d82c9f68f3ef5ee12e3010097b8d
ayselsari91 <ayselsari91@gmail.com>
2020-11-01 16:32:48 -0500

    print message

--- src/test/java/LastGitHubClass.java
+++ src/test/java/LastGitHubClass.java
@@ -2,5 +2,11 @@ public class LastGitHubClass {
 
     // i cant pull anything
 
+    public static void main(String[] args) {
+
+        System.out.println("This is GitHub and Git class");
+
+    }
+
 }
 
