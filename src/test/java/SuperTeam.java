e71a7f87556b3268f70e7f198c875f09f4c0deef
shahes <esrasahbaz@gmail.com>
2020-11-01 15:13:24 -0500

    Added new class

--- src/test/java/SuperTeam.java
+++ src/test/java/SuperTeam.java
@@ -1,7 +1,9 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 
 public class SuperTeam {
     public static void main(String[] args) {
         System.out.println("We are working on things together");
+        System.out.println("This is fun:)");
+
     }
 }
