f724c330184d80ba0f58617e5cd0e60f680cc5bd
Murat <degirmenci_murat@yahoo.com>
2020-11-01 16:29:40 -0500

    second update

--- src/test/java/LastGitHubClass_Mrt.java
+++ src/test/java/LastGitHubClass_Mrt.java
@@ -1,2 +1,14 @@
 public class LastGitHubClass_Mrt {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Allllrayddddiiii");
+
+        System.out.println("I learned a lot git and github");
+
+    }
+
+
 }
